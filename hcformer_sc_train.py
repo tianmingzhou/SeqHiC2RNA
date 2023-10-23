ed9db31dd8e9e470a3c8c1e92ee2dc7629d35464
zhhhhahahaha <760213889@qq.com>
2023-10-23 22:20:10 +0000

    first version of single cell prediction

---
+++ hcformer_sc_train.py
@@ -0,0 +1,234 @@
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import argparse
+import numpy as np
+import os
+import wandb
+import shutil
+
+from utils.utils import seed_all
+from utils.dataset import load_data_sc
+from algo.Hcformer_pretrain import Hcformer
+from algo.module import pearson_corr_coef, poisson_loss
+from tqdm import tqdm
+
+
+def evaluation(model, data_loader, device):
+    model.eval()
+    with torch.no_grad():
+        with tqdm(total=len(data_loader), dynamic_ncols=True) as t:
+            t.set_description('Evaluation: ')
+            total_pred = []
+            total_exp = []
+            for seq, exp, hic_1d in data_loader:
+                seq, hic_1d = seq.to(device), hic_1d.to(device)
+                pred = model(seq, head='human', hic_1d=hic_1d)
+
+                total_pred.append(pred.detach().cpu())
+                total_exp.append(exp.unsqueeze(-1))
+                t.update()
+            total_pred = torch.concat(total_pred, dim=0)
+            total_exp  = torch.concat(total_exp,  dim=0)
+
+    return pearson_corr_coef(total_pred, total_exp)[0]
+
+def train():
+    if args.use_wandb:
+        run = wandb.init()
+        lr = wandb.config.lr
+        wd = wandb.config.wd
+        depth = wandb.config.depth
+    else:
+        lr = args.lr
+        wd = args.wd
+        depth = args.depth
+
+    train_loader, valid_loader, test_loader = load_data_sc(
+        path = args.data_path, 
+        seed = args.seed, 
+        batch_size = args.batch_size, 
+        num_workers = args.num_workers, 
+        target_len = args.target_length)
+
+    model = Hcformer.from_hparams(
+        dim = args.dim,
+        seq_dim = args.dim,
+        depth = depth,
+        heads = args.heads,
+        output_heads = dict(human=args.output_heads),
+        target_length = args.target_length,
+        dim_divisible_by = args.dim / 12,
+        hic_1d_feat_dim = args.dim,
+    ).to(args.device)
+
+    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
+
+    # start training
+    print('Start training')
+    best_pearson_corr_coef = -1
+    best_epoch = 0
+    kill_cnt = 0
+    for epoch in range(args.epochs):
+        train_loss = []
+        train_pred = []
+        train_exp = []
+        model.train()
+        with tqdm(total=len(train_loader), dynamic_ncols=True) as t:
+            t.set_description(f'Epoch: {epoch+1}/{args.epochs}')
+            for seq, exp, hic_1d in train_loader:
+                seq, exp, hic_1d = seq.to(args.device), exp.to(args.device), hic_1d.to(args.device)
+                pred = model(seq, head='human', hic_1d=hic_1d)
+
+                # compute loss and metric
+                tr_loss = poisson_loss(pred, exp.unsqueeze(-1))
+                train_loss.append(tr_loss.item())
+
+                train_pred.append(pred.detach().cpu())
+                train_exp.append(exp.unsqueeze(-1).detach().cpu())
+
+                # backward
+                optimizer.zero_grad()
+                tr_loss.backward()
+                optimizer.step()
+
+                t.update()
+                t.set_postfix({
+                    'train_loss': f'{tr_loss.item():.4f}',
+                })
+        train_loss = np.mean(train_loss)
+        train_pred = torch.concat(train_pred, dim=0)
+        train_exp = torch.concat(train_exp, dim=0)
+        train_pearson_corr_coef = pearson_corr_coef(train_pred, train_exp)[0]
+
+        # validate
+        mean_valid_pearson_corr_coef = evaluation(model, valid_loader, args.device)
+
+        print("In epoch {}, Train Loss: {:.5}, Train Pearson_Corr_Coef: {:.5}, Valid Pearson_Corr_Coef: {:.5}\n".format(epoch+1, train_loss, train_pearson_corr_coef, mean_valid_pearson_corr_coef))
+        if args.use_wandb:
+            wandb.log({
+                'epoch': epoch+1,
+                'Train Loss': train_loss,
+                'Train Pearson_Corr_Coef': train_pearson_corr_coef,
+                'Valid Pearson_Corr_Coef': mean_valid_pearson_corr_coef,
+            })
+
+        if mean_valid_pearson_corr_coef > best_pearson_corr_coef:
+            best_pearson_corr_coef = mean_valid_pearson_corr_coef
+            best_epoch = epoch + 1
+            kill_cnt = 0
+            if args.use_wandb:
+                torch.save(model.state_dict(), os.path.join(args.model_save_path, run.id))
+            else:
+                torch.save(model.state_dict(), os.path.join(args.model_save_path, 'best_model'+str(args.gpu)))
+            print("saving model...")
+        else:
+            kill_cnt += 1
+            if kill_cnt >= args.early_stop:
+                print('early stop.')
+                break
+        
+    # Use the best model to test
+    model.eval()
+    if args.use_wandb:
+        model.load_state_dict(torch.load(os.path.join(args.model_save_path, run.id)))
+    else:
+        model.load_state_dict(torch.load(os.path.join(args.model_save_path, 'best_model'+str(args.gpu))))
+    mean_test_pearson_corr_coef = evaluation(model, test_loader, args.device)
+    print("Best epoch: {}, Best Valid Pearson_Corr_Coef: {:.5}, Test Pearson_Corr_Coef: {:.5}\n".format(best_epoch, best_pearson_corr_coef, mean_test_pearson_corr_coef)) # We Already plus 1 for best epoch in previous code
+    if args.use_wandb:
+        wandb.log({
+            'Best Epoch': best_epoch,
+            'Best Valid Pearson_Corr_Coef': best_pearson_corr_coef,
+            'Test Pearson_Corr_Coef': mean_test_pearson_corr_coef,
+        })
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_path', default='./data', help='Path of the dataset')
+    parser.add_argument('--out_path', default='./output', help='Path to save the output')   
+    parser.add_argument('--pretrain_vec_path', default='./data', help='Path of the pretrain vector')
+    parser.add_argument('--pretrain_path', default='./algo/pretrain', help='Path to save the pretrain parameter')
+    parser.add_argument('--use_wandb', action='store_true')
+    parser.add_argument('--use_sweep', action='store_true')
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--batch_size', default=2, type=int)
+    parser.add_argument('--num_workers', default=8, type=int)
+    parser.add_argument('--lr', default=1e-4, type=float, help='Learning Rate')
+    parser.add_argument('--wd', default=0.0, help='L2 Regularization for Optimizer')
+    parser.add_argument('--epochs', default=100, type=int)
+    parser.add_argument('--gpu', type=int, default='0', help='Set GPU Ids : Eg: For CPU = -1, For Single GPU = 0')
+    parser.add_argument('--num', type=int, default=0, help='To distinguish different sweep')
+    parser.add_argument('--early_stop', default=10, type=int, help='Patience for early stop.')
+
+    # Hcformer hyperparameters
+    parser.add_argument('--dim', default=1536, type=int)
+    parser.add_argument('--seq_dim', default=1536, type=int)
+    parser.add_argument('--depth', default=11, type=int, help='Number of transformer blocks')
+    parser.add_argument('--heads', default=8, type=int, help='Attention Heads')
+    parser.add_argument('--output_heads', default=1, type=int)
+    parser.add_argument('--target_length', default=240, type=int)
+    parser.add_argument('--hic_1d_feat_dim', default=1536, type=int)
+
+    # parallelize sweep
+    parser.add_argument('--parallelize', action='store_true')
+    parser.add_argument('--sweep_id', type=str)
+
+    args = parser.parse_args()
+    print(args)
+
+    # prepare the device
+    device = torch.device(f"cuda:{args.gpu}" if (torch.cuda.is_available() and args.gpu >= 0) else "cpu")
+    print(f"Device is {device}.")
+    args.device = device
+    
+    # prepare the output
+    args.out_path = os.path.join(args.out_path, 'hcformer_sc')
+    os.makedirs(args.out_path, exist_ok=True)
+    os.makedirs(os.path.join(args.out_path, 'model'), exist_ok=True)
+    args.model_save_path = os.path.join(args.out_path, 'model')
+
+    seed_all(args.seed)
+
+    if args.use_wandb:
+        if args.use_sweep:
+            sweep_name = 'hcformer_sc'+str(args.num)
+            sweep_configuration = {
+                'project': 'hcformer_sc',
+                'method': 'random',
+                'name': sweep_name,
+                'parameters':{
+                    'lr':{
+                        'values': [1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6],
+                    },
+                    'wd':{
+                        'values': [1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 1e-7],
+                    },
+                    'depth':{
+                        'distribution': 'int_uniform',
+                        'min': 11,
+                        'max': 11,
+                    }
+                }
+            }
+            args.model_save_path = os.path.join(args.model_save_path, sweep_name)
+            if os.path.exists(args.model_save_path):
+                shutil.rmtree(args.model_save_path)
+            os.mkdir(args.model_save_path)
+            sweep_id = wandb.sweep(sweep=sweep_configuration, project='hcformer_sc')
+            wandb.agent(sweep_id, function=train)
+        elif args.parallelize:
+            args.model_save_path = os.path.join(args.model_save_path, 'hcformer_sc'+str(args.num))
+            if not os.path.exists(args.model_save_path):
+                os.mkdir(args.model_save_path)
+            wandb.agent(sweep_id=args.sweep_id, function=train)
+    else:
+        train()
+
+
+
+
+
+
+
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any
 import torch
 import numpy as np
 
@@ -51,15 +52,82 @@ class bulk_mBC_hic1d(Dataset):
         return self.seq.shape[0]
 
 class sc_mBC(Dataset):
-    def __init__(self, ):
-        pass
+    def __init__(self, seq, seq_indice, exp, hic_1d):
+        self.seq = seq
+        self.seq_indice = seq_indice
+        self.exp = exp
+        self.hic_1d = hic_1d
+
+    def __getitem__(self, index):
+        seq_indice = self.seq_indice[index]
+        seq = self.seq[seq_indice]
+        exp = torch.tensor(self.exp[index])
+        hic_1d = torch.tensor(self.hic_1d[index])
+
+        return seq, exp.float(), hic_1d.float()
         
 
 
 def load_data_sc(path, seed, batch_size, num_workers, target_len):
     total_sequences = read_DNAseq_tsv_enf(os.path.join(path, 'sequence_1024_200.tsv'))
-    total_expressions = read_Expre_mtx(os.path.join(path, 'expression_cov_1024_200.mtx'))
-    
+    total_expressions = read_Expre_mtx(os.path.join(path, 'expression_cov_1024_200.mtx')).X.toarray()
+    total_ab_score = read_1D_HiC(os.path.join(path, '1d-score-10kb-ab_1024_200.pkl')).reshape(-1, 400, 1)
+    total_ins_score_25 = read_1D_HiC(os.path.join(path, '1d-score-10kb-is-hw25_1024_200.pkl')).reshape(-1, 400, 1)
+    total_ins_score_50 = read_1D_HiC(os.path.join(path, '1d-score-10kb-is-hw50_1024_200.pkl')).reshape(-1, 400, 1)
+    total_ins_score_100 = read_1D_HiC(os.path.join(path, '1d-score-10kb-is-hw100_1024_200.pkl')).reshape(-1, 400, 1)
+    total_genebody = read_1D_HiC(os.path.join(path, '1d-score-10kb-genebody_1024_200.pkl')).reshape(-1, 400, 1)
+
+    # Normalize the Expression data
+    row_min = np.min(total_expressions, axis=1, keepdims=True)
+    row_max = np.max(total_expressions, axis=1, keepdims=True)
+    total_expressions = (total_expressions - row_min) / (row_max - row_min)
+    total_expressions = np.log1p(total_expressions * 1e4)
+    total_expressions = total_expressions.reshape(-1, 400)
+
+    # crop the DNA-sequence from two sides
+    trim = (target_len - total_expressions.shape[1]) // 2
+    total_expressions = total_expressions[:, -trim:trim]
+
+    # transform the 1D HiC data
+    total_1D_HiC = np.concatenate((total_ab_score, total_ins_score_25, total_ins_score_50, total_ins_score_100, total_genebody), axis=2)
+
+    # generate random indice
+    k = int(total_expressions.shape[0]/100)
+    indice = np.zeros((total_expressions.shape[0], 1))
+    indice[:k] = 1
+    indice[k:2*k] = 2
+    np.random.seed(seed)
+    indice = np.random.permutation(indice)
+    train_indice = np.where(indice==0)[0]
+    valid_indice = np.where(indice==2)[0]
+    test_indice = np.where(indice==1)[0]
+
+    train_seq_indice = train_indice % 3740
+    valid_seq_indice = valid_indice % 3740
+    test_seq_indice = test_indice % 3740
+
+    # split the data
+    train_exp = total_expressions[train_indice]
+    train_1d_hic = total_1D_HiC[train_indice]
+
+    valid_exp = total_expressions[valid_indice]
+    valid_1d_hic = total_1D_HiC[valid_indice]
+
+    test_exp = total_expressions[test_indice]
+    test_1d_hic = total_1D_HiC[test_indice]
+
+    train_dataset = sc_mBC(total_sequences, train_seq_indice, train_exp, train_1d_hic)
+    valid_dataset = sc_mBC(total_sequences, valid_seq_indice, valid_exp, valid_1d_hic)
+    test_dataset = sc_mBC(total_sequences, test_seq_indice, test_exp, test_1d_hic)
+
+    train_loader = DataLoader(
+        dataset = train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    valid_loader = DataLoader(
+        dataset = valid_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    test_loader = DataLoader(
+        dataset = test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, valid_loader, test_loader
 
 
 def load_data_bulk_pretrain(path,pretrain_vec_path, seed, batch_size, num_workers, target_len):
